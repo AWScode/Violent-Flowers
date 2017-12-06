a83bf6649b311c3a5ae636aecbd30928778102d0
MissCA <31779111+MissCA@users.noreply.github.com>
2017-12-06 13:45:59 -0800

    Add files via upload

---
+++ Binary.java
@@ -0,0 +1,24 @@
+/*
+Chloe Arranza
+IB Computer Science
+December 6, 2017
+Binary Project
+*/
+
+import java.util.*;
+import java.util.Scanner;
+
+public class BinaryOperations{
+  public static void main(String[] args) {
+    int left = Integer.parseInt(first, 2);
+    int right = Integer.parseInt(second, 2);
+
+    Scanner in = new Scanner(System.in);
+    System.out.println("Please input your first binary number: ");
+    String first = in.nextLine();
+    System.out.println("Now please input your second binary number: ");
+    String second = in.nextLine();
+    System.out.println("Thank you, here is the sum of your binary numbers: ");
+  }
+
+}
