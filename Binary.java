62cea03b2f4cefa5e9852389a7c62d0b7f20ca01
MissCA <31779111+MissCA@users.noreply.github.com>
2017-12-11 13:43:51 -0800

    Update Binary.java

--- Binary.java
+++ Binary.java
@@ -5,20 +5,33 @@ December 6, 2017
 Binary Project
 */
 
-import java.util.*;
-import java.util.Scanner;
+import java.util.*; // used in order to import items from the utility class
+import java.util.Scanner; // used in order to import the scanner class
 
 public class BinaryOperations{
   public static void main(String[] args) {
-    int left = Integer.parseInt(first, 2);
-    int right = Integer.parseInt(second, 2);
 
-    Scanner in = new Scanner(System.in);
-    System.out.println("Please input your first binary number: ");
-    String first = in.nextLine();
-    System.out.println("Now please input your second binary number: ");
-    String second = in.nextLine();
-    System.out.println("Thank you, here is the sum of your binary numbers: ");
+    int left[] = new int[7];
+    int right[] = new int[7];
+
+
+    System.out.println("Please input your first binary number: ");// where the person inserts 1st binary
+    Scanner left[]= new Scanner(System.in);
+    
+
+    System.out.println("Now please input your second binary number: ");// where the person inserts 2nd binary
+    Scanner right[]= new Scanner(System.in);
+
+    System.out.println("Thank you, here is the sum of your binary numbers: ");// the final result
+
+
+    public static int addBinary(){}// method to add the binaries
+
+    public static int subtractBinary(){} // method to subtract the binaries
+
+    public static int multiplyBinary(){} // method to multiply the binaries
+
+    public static int divideBinary(){}   // method to divide the binaries
   }
 
 }
