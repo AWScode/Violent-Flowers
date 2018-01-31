aabd7d4ad323aa3a4b3430428f20dcfe26492692
MissCA <31779111+MissCA@users.noreply.github.com>
2018-01-31 13:36:41 -0800

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,13 +1,27 @@
+/*
+Chloe Arranza
+Factorial
+January 30, 2018
+*/
+import java.util.*;
+
+
 public class Factorial{
 
     public static void main(String[] args) {
+      System.out.println("Hello, please insert a number you want the factorial of");
+      Scanner help = new Scanner(System.in);
+      int baseNum = help.nextInt();
+      
 
-        int number = 10;
+      //  int number = 10;
         long fact = 1;
-        for(int i = 1; i <= number; i++)
+        for(int i = 1; i <= baseNum; i++)
+          //for(start with, go up to, keep looping)
         {
             fact = fact * i;
         }
-        System.out.println("The factorial of "+number+" is: "+fact);
+        System.out.println("The factorial of "+baseNum+" is: "+fact);
+        //always make sure to print
     }
 }
