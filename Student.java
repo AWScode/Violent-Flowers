c52881b8917e6e7008f156ed6017fffbc1bfa894
MissCA <31779111+MissCA@users.noreply.github.com>
2018-02-12 14:25:49 -0800

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,26 @@
+/*
+Chloe Arranza
+Februrary 12, 2018
+Student
+*/
+
+public class Student{
+
+  private static String name;
+  private static int age;
+  private static int house;
+
+  public Student(String n, int a, int h) {
+
+      this.name = n;
+      this.age = a;
+      this.house = h;
+
+  }
+
+  public String getName(){
+      return this.name;
+  }
+
+  
+}
