519225c20a86e542281abc3abe2e52a2531a002b
MissCA <31779111+MissCA@users.noreply.github.com>
2018-02-14 11:15:46 -0800

    Create Subject.java

---
+++ Subject.java
@@ -0,0 +1,31 @@
+/*
+Chloe Arranza
+Februrary 14, 2018
+Subject.java
+*/
+
+public class Subject{
+
+  private static String name;
+  private static String teacher;
+  private static int period;
+
+  public Subject(String n, String t, int p) {
+
+    this.name = n;
+    this.teacher = t;
+    this.period = p;
+  }
+
+  public String getName(){
+    return.this.name;
+  }
+
+  public String getTeacher(){
+    return.this.teacher;
+  }
+
+  public int getPeriod(){
+    return.this.period;
+  }
+}
