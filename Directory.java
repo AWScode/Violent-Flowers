c6b4a00fd5b5c015209353a00e13fd1193f31d5b
MissCA <31779111+MissCA@users.noreply.github.com>
2018-02-14 13:33:34 -0800

    Create Directory.java

---
+++ Directory.java
@@ -0,0 +1,24 @@
+/*
+Chloe Arranza
+Directory,java
+Februrary 14,2018
+*/
+
+import java.util.*;
+
+
+public class Directory{
+
+ public static void main(String[] args) {
+    System.out.println("Welcome to Hogwarts, the school for the best wizards and witches.");
+    System.out.println("Here we have many exciptional students");
+
+
+    //figure out how to put a space between names
+    Student =
+
+    System.out.println("Actually, if you would like to talk to a student, I can call them in.")
+  }
+
+
+}
