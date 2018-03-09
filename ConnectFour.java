97576786a3bebb81e6b42f74ea2fb84561cba517
MissCA <31779111+MissCA@users.noreply.github.com>
2018-03-09 13:12:35 -0800

    Update ConnectFour.java

--- ConnectFour.java
+++ ConnectFour.java
@@ -5,30 +5,32 @@ Connect Four
 */
 
 import java.util.Scanner;
- 
+
 public class ConnectFour {
- 
-  public static String[][] createPattern() {
- 
+
+//here is the actual board (which is changed to look more like the board rather than 0 and 1)
+  public static String[][] createBoard() {
+
     String[][] f = new String[7][15];
- 
+
     for (int i =0;i<f.length;i++) {
- 
+
        for (int j =0;j<f[i].length;j++) {
         if (j% 2 == 0){
            f[i][j] ="|";
         } else {
            f[i][j] = " ";
         }
- 
+
         if (i==6) f[i][j]= "-";
       }
- 
+
     }
     return f;
   }
- 
-  public static void printPattern(String[][] f) {
+
+//here we show the board on the screen
+  public static void printBoard(String[][] f) {
     for (int i =0;i<f.length;i++) {
       for (int j=0;j<f[i].length;j++) {
         System.out.print(f[i][j]);
@@ -36,9 +38,10 @@ public class ConnectFour {
       System.out.println();
     }
   }
- 
-  public static void dropRedPattern(String[][] f) {
-    System.out.print("Drop a red disk at column (0–6): ");
+
+//prompts the red user to drop a red disk
+  public static void dropRedBoard(String[][] f) {
+    System.out.print("Please drop a red disk at column (0–6): ");
     Scanner scan = new Scanner (System.in);
     int c = 2*scan.nextInt()+1;
     for (int i =5;i>=0;i--) {
@@ -48,35 +51,38 @@ public class ConnectFour {
       }
     }
   }
- 
-  public static void dropBlackPattern(String[][] f) {
-    System.out.print("Drop a black disk at column (0–6): ");
+
+//prompts the green user to drop a green disk
+  public static void dropGreenBoard(String[][] f) {
+    System.out.print("Please drop a green disk at column (0–6): ");
     Scanner scan = new Scanner (System.in);
     int c = 2*scan.nextInt()+1;
     for (int i =5;i>=0;i--) {
       if (f[i][c] == " ") {
-        f[i][c] = "B";
+        f[i][c] = "G";
         break;
       }
     }
   }
- 
- 
-  public static String checkWinner(String[][] f) {
+
+
+//here we check four
+  public static String checkFour(String[][] f) {
     for (int i =0;i<6;i++) {
       for (int j=0;j<7;j+=2) {
         if ((f[i][j+1] != " ")
+        //searched how to use && "&& is logical. & evaluates both sides of the operation"
         && (f[i][j+3] != " ")
         && (f[i][j+5] != " ")
         && (f[i][j+7] != " ")
         && ((f[i][j+1] == f[i][j+3])
         && (f[i][j+3] == f[i][j+5])
         && (f[i][j+5] == f[i][j+7])))
- 
-          return f[i][j+1]; 
+
+          return f[i][j+1];
       }
     }
- 
+
     for (int i=1;i<15;i+=2) {
       for (int j =0;j<3;j++) {
             if((f[j][i] != " ")
@@ -86,12 +92,12 @@ public class ConnectFour {
             && ((f[j][i] == f[j+1][i])
             && (f[j+1][i] == f[j+2][i])
             && (f[j+2][i] == f[j+3][i])))
-              return f[j][i]; 
-      } 
+              return f[j][i];
+      }
     }
- 
+
     for (int i=0;i<3;i++) {
- 
+
       for (int j=1;j<9;j+=2) {
             if((f[i][j] != " ")
             && (f[i+1][j+2] != " ")
@@ -100,10 +106,10 @@ public class ConnectFour {
             && ((f[i][j] == f[i+1][j+2])
             && (f[i+1][j+2] == f[i+2][j+4])
             && (f[i+2][j+4] == f[i+3][j+6])))
-              return f[i][j]; 
-      } 
+              return f[i][j];
+      }
     }
- 
+
     for (int i=0;i<3;i++) {
       for (int j=7;j<15;j+=2) {
             if((f[i][j] != " ")
@@ -113,31 +119,35 @@ public class ConnectFour {
             && ((f[i][j] == f[i+1][j-2])
             && (f[i+1][j-2] == f[i+2][j-4])
             && (f[i+2][j-4] == f[i+3][j-6])))
-              return f[i][j]; 
-      } 
+              return f[i][j];
+      }
     }
- 
+
     return null;
   }
- 
+
+//main
   public static void main (String[] args) {
-    String[][] f = createPattern();
+    String[][] f = createBoard();
     boolean loop = true;
     int count = 0;
-    printPattern(f);
+    printBoard(f);
     while(loop) {
-      if (count % 2 == 0) dropRedPattern(f);
-      else dropBlackPattern(f);
+      if (count % 2 == 0) dropRedBoard(f);
+      else dropGreenBoard(f);
       count++;
-      printPattern(f);
-      if (checkWinner(f) != null) {
-        if (checkWinner(f) == "R"){
-           System.out.println("Red won.");
-        } else if (checkWinner(f)== "B") {
-          System.out.println("Black won.");
+      printBoard(f);
+      //what happens when the either player has one
+      if (checkFour(f) != null) {
+        if (checkFour(f) == "R"){
+           System.out.println("Congrats, the red player has won!");
+        } else if (checkFour(f)== "G") {
+          System.out.println("Congrats, the green player has won!");
         }
         loop = false;
       }
     }
   }
 }
+
+//and here we have reached the end of the code
